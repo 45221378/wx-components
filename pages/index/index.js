196c59b9a6ae8540f839a6520d5b526b655c64a2
meiyulu <576649583@qq.com>
2020-08-27 16:58:57 +0800

    ‘提交圆形进度条组件‘ git commit -m ‘ ’提交圆形进度条组件

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    circleList: [
+      {id:'circwewle1',precent:60,r:40},
+      {id:'circwewle2',precent:17,r:50},
+      {id:'circwewle3',precent:26,r:60},
+      {id:'circwewle4',precent:92,r:80},
+      {id:'circwewle5',precent:36,r:70}
+    ]
+  },
+  //事件处理函数
+  
+  onLoad: function () {
+     
+  },
+  
+})
